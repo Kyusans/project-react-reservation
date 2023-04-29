5f756e00f0075ffbd34578d1a00ccff3916d27fe
Mel Macario <xmelmacario@gmail.com>
2023-04-29 11:43:12 +0800

    added login modal

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 import { useState } from "react";
-import { Card, Container, FloatingLabel, Form, Button } from "react-bootstrap";
+import { Card, Container, FloatingLabel, Form, Button, Modal } from "react-bootstrap";
 import AlertScript from "./AlertScript";
 
-const Login = () => {
+const Login = (props) => {
   const [empId, setEmpId] = useState("");
   const [password, setPassword] = useState("");
   const [showInvalid, setShowInvalid] = useState(false);
+  const {show, onHide} = props;
   //for alert
 	const [showAlert, setShowAlert] = useState(false);
 	const [alertVariant, setAlertVariant] = useState("");
@@ -28,42 +29,55 @@ const Login = () => {
       setShowInvalid(false);
       if(res.data !== 0){
         getAlert("success", "Success!");
+        localStorage.setItem("isLoggedIn", "1");
+        console.log("Local Storage: " + localStorage.getItem("isLoggedIn"));
+        setTimeout(()=>{window.location.reload();}, 1250);
       }else{
-        setTimeout(() => {setShowInvalid(true)}, 300)
+        setTimeout(() => {setShowInvalid(true);}, 300);
       }
     })
     .catch((err)=>{
       getAlert("There was an error: " + err);
     })
   }
-  return ( 
+  const handleOnHide = () =>{
+    setEmpId("");
+    setPassword("");
+    setShowAlert(false);
+    setShowInvalid(false);
+    onHide();
+  }
+  return (
     <>
-      <Container className="w-50 mt-2 text-center">
-        <AlertScript show={showAlert} variant={alertVariant} message={alertMessage} />
-      </Container>
-      <Container fluid="md" className="centered">
-        <Form className="text-center">
-
-          <Card className="card-thin border-dark">
-            <Card.Body>
-              <h2 className="text-center mt-4">Login</h2>
-
-              <Form.Group className="mt-3 mb-3">
-                <FloatingLabel label="User Id">
-                  <Form.Control type="text" value={empId} onChange={(e) => setEmpId(e.target.value)} placeholder="User Id" autoFocus required/>
-                </FloatingLabel>
-              </Form.Group>
-              <Form.Group>
-                <FloatingLabel label="Password">
-                  <Form.Control type="text" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" required/>
-                </FloatingLabel>
-                {showInvalid && <Form.Text className="text-danger">Invalid login credentials</Form.Text>}
-              </Form.Group>
-              <Button className="button-large mt-3 big-height" variant="outline-success" onClick={login}>Login</Button>
-            </Card.Body>
-          </Card>
-        </Form>
-      </Container>
+      <Modal show={show} onHide={onHide} fullscreen={true}>
+        <Modal.Body>
+          <Container className="w-50 mt-2 text-center">
+            <AlertScript show={showAlert} variant={alertVariant} message={alertMessage} />
+          </Container>
+          <Container fluid="md" className="centered">
+            <Form className="text-center">
+              <Card className="card-thin border-dark">
+                <Card.Body>
+                  <h2 className="text-center mt-5">Login</h2>
+                  <Form.Group className="mt-3 mb-3">
+                    <FloatingLabel label="User Id">
+                      <Form.Control type="text" value={empId} onChange={(e) => setEmpId(e.target.value)} placeholder="User Id" autoFocus required/>
+                    </FloatingLabel>
+                  </Form.Group>
+                  <Form.Group>
+                    <FloatingLabel label="Password">
+                      <Form.Control type="text" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" required/>
+                    </FloatingLabel>
+                    {showInvalid && <Form.Text className="text-danger">Invalid login credentials</Form.Text>}
+                  </Form.Group><hr />
+                  <Button className="button-large mt-2 big-height" variant="outline-success" onClick={login}>Login</Button>
+                  <Button className="button-large mt-3 big-height" variant="outline-danger" style={{ width: "75px" }} onClick={handleOnHide}>Back</Button>
+                </Card.Body>
+              </Card>
+            </Form>
+          </Container>
+        </Modal.Body>
+      </Modal>
     </>
   );
 }
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,17 +1,31 @@
 import { NavLink, Navbar} from "react-bootstrap";
-import "../index.css";
+import "./../index.css";
+import { useState } from "react";
+import Login from "./Login";
 
 function NavBar() {
+  console.log("nav now Local Storage: " + localStorage.getItem("isLoggedIn"));
+  const [showLoginModal, setShowLoginModal] = useState(false);
+  const openLoginModal = () =>{setShowLoginModal(true);}
+  const closeLoginModal = () =>{setShowLoginModal(false);}
+  const handleLogOut = () => {
+    localStorage.setItem("isLoggedIn", 0);
+    setTimeout(() => {window.location.reload();}, 800);
+  }
   return (
     <>
       <Navbar className="nav-background" expand="lg" text="light">
-          <Navbar.Brand className="brand">IT Days 2023</Navbar.Brand>
+          <Navbar.Brand className="brand">Reservation</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav "/>
           <Navbar.Collapse className="brand" id="basic-navbar-nav">
-          <NavLink style={{color: "white", marginRight: "10px"}} href="/">Home</NavLink>
+          <NavLink className="link-text" href="/">Home</NavLink>
+          {localStorage.getItem("isLoggedIn") !== "1" ? (<NavLink className="link-text ms-auto" onClick={openLoginModal}>Login</NavLink>) 
+            :(<NavLink className="link-text ms-auto" onClick={handleLogOut}>Logout</NavLink>)
+          }
+
           </Navbar.Collapse>
       </Navbar>
-
+      <Login show={showLoginModal} onHide={closeLoginModal} />
     </>
   );
 }
