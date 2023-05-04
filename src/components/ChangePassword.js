c85c5c4d1f09a17ae67d9ddc2e1baafd97a39d03
Mel Macario <xmelmacario@gmail.com>
2023-05-04 14:32:12 +0800

    added better alert in change password script

--- src/components/ChangePassword.js
+++ src/components/ChangePassword.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useState } from "react";
 import { Card, Form, FloatingLabel, Container, Button } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
+import AlertScript from "./AlertScript";
 
 const ChangePassword = () => {
   const [validated, setValidated] = useState(false);
@@ -9,6 +10,17 @@ const ChangePassword = () => {
   const [newPassword, setNewPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const navigateTo = useNavigate();
+
+  	//for alert
+	const [showAlert, setShowAlert] = useState(false);
+	const [alertVariant, setAlertVariant] = useState("");
+	const [alertMessage, setAlertMessage] = useState("");
+
+	function getAlert(variantAlert, messageAlert){
+		setShowAlert(true);
+		setAlertVariant(variantAlert);
+		setAlertMessage(messageAlert);
+	}
 
   const changePassword = () =>{
     const url = sessionStorage.getItem("url") + "/users.php";
@@ -20,17 +32,17 @@ const ChangePassword = () => {
     axios({url: url, data: formData, method: "post"})
     .then((res)=>{
       if(res.data === -1){
-        alert("Invalid current password");
+        getAlert("danger","The current password you entered is incorrect");
         setOldPassword("");
       }else if(res.data !== 0){
-        alert("success", "Success!");
+        getAlert("success", "Success!");
         setTimeout(() => {navigateTo("/");}, 1000);
       }else{
-        alert("invalid password")
+        getAlert("danger","invalid password")
       }
     })
     .catch((err)=>{
-      alert("There was an error: " + err);
+      getAlert("danger","There was an error: " + err);
     })
   }
   const formValidation = (e) =>{
@@ -39,8 +51,10 @@ const ChangePassword = () => {
       e.preventDefault();
       e.stopPropagation();
     }else if(newPassword !== confirmPassword){
-      alert("confirm password should be thesame");
+      getAlert("danger","Password confirmation does not match");
       setConfirmPassword("");
+      e.preventDefault();
+      e.stopPropagation();
     }else{
       e.preventDefault();
       e.stopPropagation();
@@ -54,19 +68,25 @@ const ChangePassword = () => {
   return ( 
     <>
       <Container fluid="md" className="mt-3" >
+        <Container className="text-center">
+          <AlertScript show={showAlert} variant={alertVariant} message={alertMessage} />
+        </Container>
         <Card border="dark">
           <Card.Header><h3>Change password</h3></Card.Header>
           <Form validated={validated} noValidate onSubmit={formValidation}>
             <Card.Body>
-                <FloatingLabel className="mb-4" label="Current password">
-                  <Form.Control type="password" placeholder="Current password" value={oldPassword} onChange={(e)=>setOldPassword(e.target.value)} required/>
-                </FloatingLabel>
-                <FloatingLabel className="mb-4" label="New password">
-                  <Form.Control type="password" placeholder="New password" value={newPassword} onChange={(e)=>setNewPassword(e.target.value)} required/>
-                </FloatingLabel>
-                <FloatingLabel className="mb-4" label="Confirm password">
-                  <Form.Control type="password" placeholder="Confirm password" value={confirmPassword} onChange={(e)=>setConfirmPassword(e.target.value)} required/>
-                </FloatingLabel>
+              <FloatingLabel className="mb-4" label="Current password">
+                <Form.Control type="password" placeholder="Current password" value={oldPassword} onChange={(e)=>setOldPassword(e.target.value)} required/>
+                <Form.Control.Feedback type="invalid">This field is required</Form.Control.Feedback>
+              </FloatingLabel>
+              <FloatingLabel className="mb-4" label="New password">
+                <Form.Control type="password" placeholder="New password" value={newPassword} onChange={(e)=>setNewPassword(e.target.value)} required/>
+                <Form.Control.Feedback type="invalid">This field is required</Form.Control.Feedback>
+              </FloatingLabel>
+              <FloatingLabel className="mb-4" label="Confirm password">
+                <Form.Control type="password" placeholder="Confirm password" value={confirmPassword} onChange={(e)=>setConfirmPassword(e.target.value)} required/>
+                <Form.Control.Feedback type="invalid">This field is required</Form.Control.Feedback>
+              </FloatingLabel>
             </Card.Body>
             <Card.Footer>
               <Container className="text-center"><Button type="submit" variant="outline-primary">Update password</Button></Container>
