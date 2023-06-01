f90e34ec35a7ca5287458feb38795c82c8a45e94
Mel Macario <xmelmacario@gmail.com>
2023-06-01 10:58:09 +0800

    added getReservation function

--- src/components/AdminResTable.js
+++ src/components/AdminResTable.js
@@ -1,12 +1,28 @@
+import axios from "axios";
+import { useState } from "react";
 import { Table } from "react-bootstrap";
 
 const AdminResTable = () => {
+  const [reservation, setReservation] = useState([]);
+
+  const getReservation = (reserveType) => {
+    const url = sessionStorage.getItem("url");
+    const jsonData = {reserveType: reserveType};
+    const formData = new FormData();
+    formData.append("operation", "getReservation");
+    formData.append("json", JSON.stringify(jsonData));
+    axios({url: url, data: formData, method: "post"})
+    .then((res)=>{res.data !== 0 ? setReservation(res.data) : alert("There is no reservation");})
+    .catch((error)=>{alert("There was an unexpected error occured: " + error);})
+  }
   return ( 
     <>
       <Table>
-          <tr>
-            <td></td>
-          </tr>
+        <tr>
+          <th>Purpose</th>
+          <th>Start Date</th>
+          <th>End Date</th>
+        </tr>
       </Table>
     </>
   );
